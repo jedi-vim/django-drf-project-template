c18a71c5d6821a391349ed0a37a7ee32474a781e
Leonam Silva <leonam.silva.cc@gmail.com>
2021-10-13 15:02:56 -0300

    Ajustar os settings para um unico modo de leitura do DATABASE_URL

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,16 +1,3 @@
-import os
-
 from .base import *
 
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ecommerce_backend_db',
-        'USER': 'ecommerce_backend_user',
-        'PASSWORD': 'IWKdrWWnc6XA39pgX6PU',
-        'HOST': 'localhost',
-        'PORT': '5453'
-    }
-}
+TEST_RUNNER = "django.test.runner.DiscoverRunner"
